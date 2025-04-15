5449b7039332b94671e7eb1be85ff4e187cbd25e
ploymel <ploy_mel@hotmail.com>
2025-04-15 12:10:44 +0700

    initial code

---
+++ src/App.js
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
+    </div>
+  );
+}
+export default App;
